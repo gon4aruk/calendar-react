b1b0ea362c60568d026b589a3b14215348c47b47
gon4aruk <sashagonch27@gmail.com>
2021-04-07 14:35:35 +0300

    add _redirects & fix redline

--- src/components/redLine/RedLine.jsx
+++ src/components/redLine/RedLine.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './redLine.scss';
 
 const RedLine = () => {
-  const [top, setTop] = useState(null);
+  const [top, setTop] = useState(new Date().getMinutes());
 
   useEffect(() => {
     const interval = setInterval(() => setTop(new Date().getMinutes()), 1000);
